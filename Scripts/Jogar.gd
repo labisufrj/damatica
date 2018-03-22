08f42a7dad4fb3ebdd9aa209273c5faea9319dd3
João de Lacerda <jpedrodelacerda@gmail.com>
2018-03-21 22:29:33 -0300

    Recuperando o real repo do Damática

---
+++ Scripts/Jogar.gd
@@ -0,0 +1,18 @@
+extends Button
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+	# Called every time the node is added to the scene.
+	# Initialization here
+	pass
+
+#func _process(delta):
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+#	var scene = preload("res://Board.tscn")
+#	var node = scene.instance()
+#	add_child(node)	
+#	pass
